f6d26e53e58652d975242399c8e09f46b13a9fd6
The Dumb <info@hemisphera.com>
2026-03-18 09:47:48 +0100

    Remove local buildscript

--- Scripts/build.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish -r win-x64 -c Release
-Copy-Item -Path "$PSScriptRoot\..\Hsp.Zulbert\bin\Release\net10.0\win-x64\publish\reaper_zulbert.dll" -Destination "$env:APPDATA\REAPER\UserPlugins" -Force
